50b1e14fa072ee1e4266098137ebcfdd54946bfd
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-23 00:27:30 +0200

    try changing port

--- Program.cs
+++ Program.cs
@@ -7,20 +7,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MarshallAPI
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
+namespace MarshallAPI {
+    public class Program {
+        public static void Main (string[] args) {
+            CreateHostBuilder (args).Build ().Run ();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder => {
+                var port = Environment.GetEnvironmentVariable ("PORT");
+                webBuilder.UseStartup<Startup> ()
+                    .UseUrls ("http://*:" + port);
+            });
     }
 }
