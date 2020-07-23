982bff44b3bc71901d1f711dc7cf066e40b02172
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-23 18:28:28 +0200

    add roles

---
+++ Entities/Role.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace MarshallAPI.Entities {
+    public static class Role {
+        public const string Admin = "Admin";
+        public const string Marshal = "Marshal";
+        public const string Driver = "Driver";
+
+    }
+}
