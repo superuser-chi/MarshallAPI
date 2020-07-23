2bbe01a400adffe4ace7032c7991def0b51eb9e7
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-23 18:30:42 +0200

    ignore nav property user

--- Entities/Kombi.cs
+++ Entities/Kombi.cs
@@ -4,14 +4,15 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace MarshallAPI.Entities
-{
-    public class Kombi
-    {
+namespace MarshallAPI.Entities {
+    public class Kombi {
         public string KombiId { get; set; }
         public string Plate { get; set; }
         public string UserId { get; set; }
+
+        [JsonIgnore]
         public User User { get; set; }
+
         [JsonIgnore]
         public ICollection<Slot> Slots { get; set; }
     }
