bb20188a61a8594b05bb30299d186af2078aed0c
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-23 02:13:40 +0200

    configure port

--- Program.cs
+++ Program.cs
@@ -16,9 +16,13 @@ namespace MarshallAPI {
         public static IHostBuilder CreateHostBuilder (string[] args) =>
             Host.CreateDefaultBuilder (args)
             .ConfigureWebHostDefaults (webBuilder => {
-                var port = Environment.GetEnvironmentVariable ("PORT");
                 webBuilder.UseStartup<Startup> ()
-                    .UseUrls ("http://*:" + port);
+                    .UseKestrel ((context, options) => {
+                        var port = Environment.GetEnvironmentVariable ("PORT");
+                        if (!string.IsNullOrEmpty (port)) {
+                            options.ListenAnyIP (int.Parse (port));
+                        }
+                    });
             });
     }
 }
