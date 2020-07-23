08dec3afc1b0642e3965f9a5ba475a7e6eb4847d
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-23 18:31:12 +0200

    add role and token

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,13 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
 
-namespace MarshallAPI.Entities
-{
-    [Table("AspNetUsers")]
-    public partial class User : IdentityUser
-    {
+namespace MarshallAPI.Entities {
+    [Table ("AspNetUsers")]
+    public partial class User : IdentityUser {
         public string RouteId { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
+
+        public string Role { get; set; }
+        public string Token { get; set; }
     }
 }
