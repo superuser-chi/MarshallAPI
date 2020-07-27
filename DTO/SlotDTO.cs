5c4487b776f28a34dc73a3ff9d30f893d4280ffd
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-27 22:14:23 +0200

    add sloDTO

---
+++ DTO/SlotDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarshallAPI.Entities;
+
+namespace MarshallAPI.DTO {
+    public class SlotDTO : Slot {
+        public string OwnerName { get; set; }
+        public string KombiPlate { get; set; }
+        public string RouteName { get; set; }
+    }
+}
