f7fc8f7e0efba788eab55927087ce4fb7bc77e41
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-28 05:03:47 +0200

    fix ownerName string

--- Services/Helpers/AutoMapperProfile.cs
+++ Services/Helpers/AutoMapperProfile.cs
@@ -1,21 +1,18 @@
-﻿using AutoMapper;
-using MarshallAPI.DTO;
-using MarshallAPI.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using MarshallAPI.DTO;
+using MarshallAPI.Entities;
 
-namespace TransportRequisitionSolution.Services.Helpers
-{
-    public class AutoMapperProfile : Profile
-    {
-        public AutoMapperProfile()
-        {
-            CreateMap<Slot, SlotDTO>()
-                .ForMember(dest => dest.OwnerName, opts => opts.MapFrom(src => $"{src.kombi.User.Firstname} {src.kombi.User.Firstname}"))
-                .ForMember(dest => dest.KombiPlate, opts => opts.MapFrom(src => $"{src.kombi.Plate}"))
-                .ForMember(dest => dest.RouteName, opts => opts.MapFrom(src => $"{src.Route.From} to {src.Route.To}"));
+namespace TransportRequisitionSolution.Services.Helpers {
+    public class AutoMapperProfile : Profile {
+        public AutoMapperProfile () {
+            CreateMap<Slot, SlotDTO> ()
+                .ForMember (dest => dest.OwnerName, opts => opts.MapFrom (src => $"{src.kombi.User.Firstname} {src.kombi.User.Lastname}"))
+                .ForMember (dest => dest.KombiPlate, opts => opts.MapFrom (src => $"{src.kombi.Plate}"))
+                .ForMember (dest => dest.RouteName, opts => opts.MapFrom (src => $"{src.Route.From} to {src.Route.To}"));
         }
     }
 }
