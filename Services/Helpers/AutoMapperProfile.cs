919878440e6bd7fb76f0fd0f26f4d9bfe11de500
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-28 13:11:42 +0200

    add mapper profile

--- Services/Helpers/AutoMapperProfile.cs
+++ Services/Helpers/AutoMapperProfile.cs
@@ -9,6 +9,8 @@ using MarshallAPI.Entities;
 namespace TransportRequisitionSolution.Services.Helpers {
     public class AutoMapperProfile : Profile {
         public AutoMapperProfile () {
+            CreateMap<Kombi, KombiDTO> ()
+                .ForMember (dest => dest.OwnerName, opts => opts.MapFrom (src => $"{src.User.Firstname} {src.User.Lastname}"));
             CreateMap<Slot, SlotDTO> ()
                 .ForMember (dest => dest.OwnerName, opts => opts.MapFrom (src => $"{src.kombi.User.Firstname} {src.kombi.User.Lastname}"))
                 .ForMember (dest => dest.KombiPlate, opts => opts.MapFrom (src => $"{src.kombi.Plate}"))
