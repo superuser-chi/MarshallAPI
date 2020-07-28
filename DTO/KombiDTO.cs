cfefe5820b8f0e4a5c6f89f3bb25185e746c2337
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-28 13:11:26 +0200

    add kombi DTO

---
+++ DTO/KombiDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MarshallAPI.Entities;
+
+namespace MarshallAPI.DTO {
+    public class KombiDTO : Slot {
+        public string OwnerName { get; set; }
+    }
+}
