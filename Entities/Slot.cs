07b7534ea25569bbbe9b981fdaa42384044397b5
Gift Mavuso <mavusogiftntokozo@gmail.com>
2020-07-28 02:42:44 +0200

    add time field

--- Entities/Slot.cs
+++ Entities/Slot.cs
@@ -4,14 +4,13 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace MarshallAPI.Entities
-{
-    public class Slot
-    {
+namespace MarshallAPI.Entities {
+    public class Slot {
         public int SlotId { get; set; }
         public string KombiId { get; set; }
         public string DayId { get; set; }
         public string RouteId { get; set; }
+        public string Time { get; set; }
 
         [JsonIgnore]
         public Kombi kombi { get; set; }
